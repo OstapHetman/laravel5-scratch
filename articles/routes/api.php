ed48aec69ab23f35ca7fbe12dff3f4fcb73c0a1f
OstapHetman <prickly4528@gmail.com>
2018-11-22 17:48:59 +0200

    setup routes

--- articles/routes/api.php
+++ articles/routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List articles
+Route::get('articles', 'ArticleController@index');
+
+// List Single Article
+Route::get('article/{id}', 'ArticleController@show');
+
+// Create new Article
+Route::post('article', 'ArticleController@store');
+
+// Create new Update
+Route::put('articles', 'ArticleController@store');
+
+
+// Create new Delete
+Route::delete('articles', 'ArticleController@destroy');
